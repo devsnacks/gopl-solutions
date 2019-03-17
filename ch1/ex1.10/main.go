deac39684e393097bf2b7dfcaa4688a15f53b69c
Sebastian Keßler <sebastian.kessler@flyeralarm.com>
2019-03-17 19:54:37 +0100

    added ex1.10

---
+++ ch1/ex1.10/main.go
@@ -0,0 +1,54 @@
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Create("results.txt")
+
+	if err != nil {
+		fmt.Printf("error while creating file: %v", err)
+	}
+
+	start := time.Now()
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+
+	for range os.Args[1:] {
+		f.WriteString(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+	f.Close()
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s\n", secs, nbytes, url)
+}
